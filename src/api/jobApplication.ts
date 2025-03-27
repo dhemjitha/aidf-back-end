df8fb37a98ef0ad5ac94db9bd6a246017e99c7f1
Dulran Hemjitha <dhemjitha@gmail.com>
2025-03-27 19:50:45 +0530

    fixed: removed admin authorization

--- src/api/jobApplication.ts
+++ src/api/jobApplication.ts
@@ -5,8 +5,10 @@ import AuthorizationMiddleware from "./middleware/authorization-middleware";
 
 const jobApplicationRouter = express.Router();
 
-jobApplicationRouter.route("/").post(createJobApplication).get(ClerkExpressRequireAuth({}), AuthorizationMiddleware, getJobApplications);
-jobApplicationRouter.route("/:id")
-  .get(ClerkExpressRequireAuth({}), AuthorizationMiddleware, getJobApplicationById).delete(ClerkExpressRequireAuth({}), AuthorizationMiddleware, deleteJobApplication);
+// jobApplicationRouter.route("/").post(createJobApplication).get(ClerkExpressRequireAuth({}), AuthorizationMiddleware, getJobApplications);
+// jobApplicationRouter.route("/:id").get(ClerkExpressRequireAuth({}), AuthorizationMiddleware, getJobApplicationById).delete(ClerkExpressRequireAuth({}), AuthorizationMiddleware, deleteJobApplication);
+
+jobApplicationRouter.route("/").post(createJobApplication).get(ClerkExpressRequireAuth({}), getJobApplications);
+jobApplicationRouter.route("/:id").get(ClerkExpressRequireAuth({}), getJobApplicationById).delete(ClerkExpressRequireAuth({}), AuthorizationMiddleware, deleteJobApplication);
 
 export default jobApplicationRouter;
